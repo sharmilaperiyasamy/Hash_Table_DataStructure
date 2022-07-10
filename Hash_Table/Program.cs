3273cd5010de8b8095d3ee430e8e98bd55b7227b
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-10 22:05:31 +0530

    [Sharmila] uc1 finding frequency

---
+++ Hash_Table/Program.cs
@@ -0,0 +1,8 @@
+﻿Hash_Table.MyMapNode<int, string> hash = new Hash_Table.MyMapNode<int, string>();
+hash.add(0, "I");
+hash.add(1, "am");
+hash.add(2, "5");
+hash.add(3, "years");
+hash.add(4, "old");
+hash.add(5, "baby");
+hash.getFrequency();
