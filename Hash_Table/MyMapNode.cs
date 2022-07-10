51f59b58b809b9cfd341c594e4e27c2dcf7e50bb
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-10 22:55:25 +0530

    [Sharmila] finding frequency in paragragh

--- Hash_Table/MyMapNode.cs
+++ Hash_Table/MyMapNode.cs
@@ -6,33 +6,103 @@ using System.Threading.Tasks;
 
 namespace Hash_Table
 {
-    internal class MyMapNode<K, V>
+    public class MyMapNode<K, V>
     {
-        LinkedList<KeyValue<K, V>> list = new LinkedList<KeyValue<K, V>>();
+        int size;
+        LinkedList<KeyValue<K, V>>[] list;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.list = new LinkedList<KeyValue<K, V>>[size];
+        }
+
         public void add(K key, V value)
         {
+            int index = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> newList = getLinkedList(index);
             KeyValue<K, V> kv = new KeyValue<K, V>() { key = key, value = value };
-            list.AddLast(kv);
+            newList.AddLast(kv);
         }
-        public void getFrequency()
+        public int getFrequency(string value)
         {
             int count = 0;
             if (list == null)
             {
                 Console.WriteLine("Hash Table is empty.");
-                return;
+                return 0;
             }
-            foreach (KeyValue<K, V> item in list)
+            for (int i = 0; i < list.Length; i++)
             {
-                foreach (KeyValue<K, V> value in list)
+                LinkedList<KeyValue<K, V>> newList = getLinkedList(i);
+                foreach (KeyValue<K, V> items in newList)
                 {
-                    if (item.value.Equals(value.value))
+                    if (items.value.Equals(value))
                         count++;
                 }
-                Console.WriteLine("Count of " + item.value + " is " + count);
-                count = 0;
             }
+            Console.WriteLine("Count of " + value + " is " + count);
+            return count;
         }
+public void Remove(K key)
+{
+    int position = GetArrayPosition(key);
+    LinkedList<KeyValue<K, V>> linkedList = getLinkedList(position);
+    bool itemFound = false;
+    KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+    foreach (KeyValue<K, V> item in linkedList)
+    {
+        if (item.key.Equals(key))
+        {
+            itemFound = true;
+            foundItem = item;
+        }
+    }
+    if (itemFound)
+    {
+        linkedList.Remove(foundItem);
+    }
+}
+public int GetArrayPosition(K key)
+{
+    int index = key.GetHashCode() % size;
+    return Math.Abs(index);
+}
+public LinkedList<KeyValue<K, V>> getLinkedList(int index)
+{
+    LinkedList<KeyValue<K, V>> newList = list[index];
+    if (newList == null)
+    {
+        newList = new LinkedList<KeyValue<K, V>>();
+        list[index] = newList;
+    }
+    return newList;
+}
+public bool ContainsKey(K key)
+{
+    int position = GetArrayPosition(key);
+    LinkedList<KeyValue<K, V>> linkedList = getLinkedList(position);
+    foreach (KeyValue<K, V> item in linkedList)
+    {
+        if (item.key.Equals(key))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+public V GetValue(K key)
+{
+    int position = GetArrayPosition(key);
+    LinkedList<KeyValue<K, V>> linkedList = getLinkedList(position);
+    foreach (KeyValue<K, V> item in linkedList)
+    {
+        if (item.key.Equals(key))
+        {
+            return item.value;
+        }
+    }
+    return default(V);
+}
     }
     public struct KeyValue<K, V>
     {
--- Hash_Table/Program.cs
+++ Hash_Table/Program.cs
@@ -1,8 +1,47 @@
-﻿Hash_Table.MyMapNode<int, string> hash = new Hash_Table.MyMapNode<int, string>();
-hash.add(0, "I");
-hash.add(1, "am");
-hash.add(2, "5");
-hash.add(3, "years");
-hash.add(4, "old");
-hash.add(5, "baby");
-hash.getFrequency();
+﻿Console.WriteLine("1.uc1-Finding Frequency in sentence\n2.uc2-Finding Frequency in paragragh\n3.Remove word\nEnter your choice:");
+int option = Convert.ToInt32(Console.ReadLine());
+switch (option)
+{
+    case 1:
+
+        Hash_Table.MyMapNode<int, string> hash = new Hash_Table.MyMapNode<int, string>(5);
+        hash.add(0, "I");
+        hash.add(1, "am");
+        hash.add(2, "5");
+        hash.add(3, "years");
+        hash.add(4, "old");
+        hash.add(5, "baby");
+        hash.getFrequency("I");
+        hash.getFrequency("am");
+        hash.getFrequency("5");
+        hash.getFrequency("years");
+        break;
+    case 2:
+        string phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        string[] words = phrase.ToLower().Split(" ");
+        var dist = words.Distinct();
+        int length = 0;
+        foreach (var word in dist)
+            length++;
+        Hash_Table.MyMapNode<string, int> map = new Hash_Table.MyMapNode<string, int>(length);
+        int count = 1;
+        foreach (string word in words)
+        {
+            if (map.ContainsKey(word))
+            {
+                count = map.GetValue(word) + 1;
+                map.Remove(word);
+                map.add(word, count);
+            }
+            else
+            {
+                map.add(word, 1);
+            }
+        }
+        Console.WriteLine($"{"Frequency",20} | {"Count",10}\n");
+        foreach (string key in dist)
+        {
+            Console.WriteLine($"{key,20} | {map.GetValue(key),10}");
+        }
+        break;
+}
